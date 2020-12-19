ff285ec5d14a664b15aedfd097ebd192db18ba08
NguyenHoangHiep28 <71130020+NguyenHoangHiep28@users.noreply.github.com>
2020-12-19 18:42:47 +0700

    Lab8

---
+++ Lab8/ASM2.sql
@@ -0,0 +1,100 @@
+﻿IF DB_ID('Assignment2') IS NOT NULL
+	DROP Database Assignment2 
+ELSE 
+	CREATE DATABASE Assignment2
+GO
+USE Assignment2
+GO
+CREATE TABLE Company(
+	ComID int PRIMARY KEY,
+	ComName nvarchar(50),
+	Tel varchar(8) CHECK (DATALENGTH(Tel) = 8 AND Tel NOT LIKE '%[^0-9]%' ),
+	Address varchar(10)
+)
+DROP TABLE Company
+INSERT INTO Company VALUES
+(201, 'ASUS', '91826381','USA'),
+(202, 'LENOVO', '91823748', 'CHINA'),
+(203, 'ACER', '91724648', 'USA'),
+INSERT INTO Company VALUES (204, 'HP', '91726463', 'USA')
+GO
+CREATE TABLE Category(
+	CateID int PRIMARY KEY,
+	CateName nvarchar(50)
+)
+INSERT INTO Category VALUES 
+(101, N'Máy Tính'),
+(102, N'Điện Thoại'),
+(103, N'Máy In')
+
+GO
+CREATE TABLE Product(
+	ProductID varchar(3) PRIMARY KEY,
+	ProductName nvarchar(50),
+	Description nvarchar(100),
+	Unit nvarchar(10),
+	Price smallmoney,
+	Qty int,
+	ComID int,
+	CateID int,
+	CONSTRAINT FK_ComID FOREIGN KEY (ComID) REFERENCES Company(ComID),
+	CONSTRAINT FK_CateID FOREIGN KEY (CateID) REFERENCES Category(CateID)
+)
+DROP TABLE Product
+INSERT INTO Product VALUES
+('A01', N'Máy Tính T450', N'Máy nhập cũ', N'Chiếc',1000 ,10 ,202 ,101 ),
+('A02', N'Điện Thoại Nokia 5670', N'Điện thoại đang hot', N'Chiếc',200 ,200 ,201 ,102 ),
+('A03', N'Máy In Samsung 450', N'Máy in đang loại bình', N'Chiếc',100 ,10 ,203 ,103 )
+SELECT * FROM Product
+SELECT * FROM Company
+SELECT * FROM Category
+--4
+	--4a
+	SELECT ComID, ComName FROM Company
+	--4b
+	SELECT * FROM Product
+--5
+	--5a
+	SELECT ComID, ComName FROM Company ORDER BY ComName DESC
+	--5b
+	SELECT * FROM Product ORDER BY Price DESC
+	--5c
+	SELECT * FROM Company WHERE ComName LIKE 'ASUS'
+	--5d
+	SELECT * FROM Product WHERE Qty < 11
+	--5e
+	SELECT ProductID, ProductName, ComName FROM Product 
+	INNER JOIN Company ON Product.ComID = Company.ComID
+	WHERE ComName LIKE 'ASUS'
+--6
+	--6a
+	SELECT COUNT(ComName) AS 'ComQty' FROM Company 
+	INNER JOIN Product ON Company.ComID = Product.ComID
+	--6b
+	SELECT COUNT(ProductID) FROM Product
+	--6c
+	SELECT ComName, Count(CateName) AS 'CompanyCate' FROM((Company
+	INNER JOIN  Product ON Company.ComID = Product.ComID)
+	INNER JOIN Category ON Product.CateID = Category.CateID)
+	GROUP BY ComName
+	--6d
+	SELECT COUNT(DISTINCT CateName) AS 'Categories' FROM Category
+	INNER JOIN Product ON Category.CateID = Product.CateID
+--7
+	--7a
+	ALTER TABLE Product
+		ADD CONSTRAINT CK_ProductPrice CHECK (Price > 0)
+	--7b
+	ALTER TABLE Company
+		ADD CONSTRAINT CK_TelNum CHECK (Tel LIKE '0%' AND DATALENGTH(Tel) = 8 AND Tel NOT LIKE '%[^0-9]%')
+	--7c
+	AlTER TABLE Product
+		ADD CONSTRAINT PK_ProID PRIMARY KEY (ProductID)
+	ALTER TABLE Product 
+		ADD CONSTRAINT Fk_ComID FOREIGN KEY (ComID) REFERENCES Company(ComID)
+	ALTER TABLE Product
+		ADD CONSTRAINT Fk_CateID FOREIGN KEY (CateID) REFERENCES Category(CateID)
+--8
+	--8a
+	CREATE INDEX IX_Des ON Product(Description)
+	CREATE INDEX IX_Name ON Product(ProductName)
---
+++ Lab8/ASM3.sql
@@ -0,0 +1,93 @@
+﻿IF DB_ID ('Assignment3') IS NOT NULL
+	DROP DATABASE Assignment3
+ELSE 
+	CREATE DATABASE Assignment3
+GO
+USE Assignment3
+GO
+CREATE TABLE [Services](
+	ServicesID int PRIMARY KEY,
+	Name nvarchar(20),
+	Description nvarchar(50)
+)
+INSERT INTO [Services] VALUES
+(101, N'Trả trước', NULL),
+(102, N'Trả sau', NULL)
+GO
+CREATE TABLE CusTel(
+	CusID varchar(3),
+	tel varchar(10) PRIMARY KEY,
+	Services_ID int,
+	StartDate date,
+	EndDate date
+)
+INSERT INTO CusTel VALUES
+('A01', '0123456789', '101', '2020-09-10', '2020-11-10'),
+('A02', '0138475819', '102', '2019-10-11', NULL)
+
+INSERT INTO CusTel VALUES ('A01', '0348483145', '101', '2016-06-08', NULL)
+INSERT INTO CusTel VALUES('A03', '0394877002', '102', '2009-12-12', NULL)
+DELETE FROM CusTel WHERE CusID LIKE 'A03' 
+GO
+SELECT * FROM CusTel
+CREATE TABLE Customer(
+	Cus_ID varchar(3) PRIMARY KEY,
+	Name nvarchar(50),
+	ID varchar(9),
+	Address nvarchar(50),
+)
+INSERT INTO Customer VALUES 
+('A01', N'Nguyễn Nguyệt Nga', '123456789', N'Cầu Giấy - Hà Nội'),
+('A02', N'Hoàng Văn Hoan', '281749174', N'Thanh Xuân - Hà Nội')
+INSERT INTO Customer VALUES ('A03', N'Trần Mạnh Hùng', '817491749', N'Hà Đông - Hà Nội')
+
+--4
+	--4a
+	SELECT * FROM Customer
+	--4b
+	SELECT * FROM CusTel
+--5
+	--5a
+	SELECT * FROM Customer INNER JOIN CusTel 
+	ON Customer.Cus_ID = CusTel.CusID
+	WHERE tel LIKE '0123456789' 
+	--5b
+	SELECT * FROM Customer WHERE ID LIKE '123456789'
+	--5c
+	SELECT tel FROM CusTel INNER JOIN Customer
+	ON CusTel.CusID = Customer.Cus_ID WHERE 
+	ID LIKE '123456789'
+	--5d
+	SELECT tel, Name, StartDate FROM CusTel 
+	INNER JOIN Customer ON CusTel.CusID = Customer.Cus_ID
+	WHERE StartDate LIKE '2009-12-12'
+	--5e
+	SELECT tel, Name, Address FROM CusTel INNER JOIN 
+	Customer ON CusTel
+--6
+	--6a
+	SELECT COUNT(Cus_ID) AS Customers FROM Customer
+	--6b
+	SELECT COUNT(tel) AS Tels FROM CusTel
+	--6c
+	SELECT COUNT(tel) AS TelDetails FROM CusTel 
+	WHERE StartDate LIKE '2009-12-12'
+	--6d
+	SELECT Name, ID, Address, tel, Services_ID, StartDate, EndDate FROM Customer INNER JOIN CusTel 
+	ON CusTomer.Cus_ID = CusTel.CusID 
+--7
+	--7a
+	ALTER TABLE CusTel
+		ALTER COLUMN StartDate date NOT NULL
+	--7b
+	ALTER TABLE CusTel
+		ADD CONSTRAINT CK_StartDate CHECK (StartDate <= GETDATE())
+	--7c
+	ALTER TABLE CusTel
+		ADD CONSTRAINT CK_Tel CHECK (DATALENGTH(tel) = 10 AND tel LIKE '09%' )
+	--7d
+	ALTER TABLE CusTel
+		ADD Points int
+--8
+	--8a
+	CREATE INDEX IX_CusName ON Customer(Name)
